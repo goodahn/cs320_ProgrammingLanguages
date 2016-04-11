d99a83e759d8f866e34a1f5ea2cd60e4a3be6dbd
goodahn <ahnjaeyeong@anjaeyeong-ui-MacBook-Pro.local>
2016-04-12 04:30:40 +0900

    Lazy first order functions

---
+++ LFAE.rkt
@@ -0,0 +1,66 @@
+#lang plai
+
+(define-type LFAE
+  [num (n number?)]
+  [id (name symbol?)]
+  [add (lhs LFAE?) (rhs LFAE?)]
+  [sub (lhs LFAE?) (rhs LFAE?)]
+  [fun (param symbol?) (body LFAE?)]
+  [app (func LFAE?) (farg LFAE?)])
+
+(define-type LFAEV
+  [numV (n number?)]
+  [closureV (param symbol?) (body LFAE?) (ds DfrdSub?)]
+  [exprV (expr LFAE?) (ds DfrdSub?)])
+
+(define-type DfrdSub
+  [mtSub]
+  [aSub (param symbol?) (value LFAEV?) (rest DfrdSub?)])
+
+;; parse : sexp -> LFAE
+;; convert s-expression into LFAE
+(define (parse sexp)
+  (match sexp
+    [(? number?) (num sexp)]
+    [(? symbol?) (id sexp)]
+    [(list '+ l r) (add (parse l) (parse r))]
+    [(list '- l r) (sub (parse l) (parse r))]
+    [(list 'fun param body) (fun (first param) (parse body))]
+    [(list fun arg) (app (parse fun) (parse arg))]))
+
+;; lookup-ds : symbol DfrdSub -> LFAEV
+(define (lookup-ds s ds)
+  (type-case DfrdSub ds
+    [mtSub () (error "free identifier")]
+    [aSub (x value ds_)  (if (symbol=? s x)
+                             value
+                             (lookup-ds s ds_))]))
+;; strict : LFAEV -> LFAEV
+(define (strict lfaev)
+  (cond
+    [(exprV? lfaev) (strict (interp (exprV-expr lfaev) (exprV-ds lfaev)))]
+    [else lfaev]))
+
+;; num-op : op -> process
+(define (num-op op)
+  (lambda (x y)
+    (numV (op (numV-n (strict x)) (numV-n (strict y))))))
+
+(define num+ (num-op +))
+(define num- (num-op -))
+
+;; interp : LFAE ds -> LFAEV
+(define (interp lfae ds)
+  (type-case LFAE lfae
+    [num (n) (numV n)]
+    [id (s) (lookup-ds s ds)]
+    [add (l r) (num+ (interp l ds) (interp r ds))]
+    [sub (l r) (num- (interp l ds) (interp r ds))]
+    [fun (x body) (closureV x body ds)]
+    [app (func farg) (local [(define fval (strict (interp func ds)))
+                             (define aval (exprV farg ds))]
+                       (interp (closureV-body fval)
+                               (aSub (closureV-param fval)
+                                     aval
+                                     (closureV-ds fval))))]))
+(test (interp (parse '{{fun {x} {+ x 1}} 1}) (mtSub)) (numV 2))
