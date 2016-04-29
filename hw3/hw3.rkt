b5f803df6dc54256794d13c8e81edc03113b669b
goodahn <ahnjaeyeong@anjaeyeong-ui-MacBook-Pro.local>
2016-04-29 15:02:30 +0900

    version2

--- hw3/hw3.rkt
+++ hw3/hw3.rkt
@@ -34,21 +34,6 @@
 [(? symbol?) (id sexp)]
 [else (error 'parse "bad syntax" sexp)]))
 
-;; reverse_parse : WAE -> list of symbols
-;; change WAE to list of symbols
-(define (reverse_parse wae)
-  (type-case WAE wae
-    [num (n) n]
-    [id (name) name]
-    [add (l r) (list '+ (reverse_parse l) (reverse_parse r))]
-    [sub (l r) (list '- (reverse_parse l) (reverse_parse r))]
-    [with (i ib body) (list 'with  (list i (reverse_parse ib)) (reverse_parse body))] ))
-
-(test (reverse_parse (add (num 3) (num 5))) '{+ 3 5})
-(test (reverse_parse (add (id 'a) (num 5))) '{+ a 5})
-(test (reverse_parse (sub (num 3) (num 5))) '{- 3 5})
-(test (reverse_parse (with 'a (num 1) (num 2))) '{with {a 1} 2})
-(test (reverse_parse (with 'a (with 'a (num 1) (num 2)) (num 2))) '{with {a {with {a 1} 2}} 2})
 (test (parse '{+ 5 4}) {add (num 5) (num 4)})
 (test (parse '{- 5 4}) {sub (num 5) (num 4)})
 (test (parse '6) (num 6))
@@ -56,31 +41,22 @@
 
 ;; free-ids : WAE -> list-of-sym
 ;; find free identifiers and list them alphabetical order
-(define (free-ids wae__)
-  (define wae (reverse_parse wae__))
-  (define (tmp wae_)
-  (cond
-    [(number? wae_) '()]
-    [(symbol? wae_) (list wae_)]
-    [(list? wae_) 
-     (cond
-       [(eq? 'with (first wae_)) 
-        
-            (sort (remove-duplicates (remove* (list (first (second wae_))) (append (tmp (third wae_)) (tmp (second (second wae_)))))) symbol<?)
-           ]
-       [(eq? '+ (first wae_)) (sort (remove-duplicates (append (tmp (second wae_)) (tmp (third wae_)))) symbol<?)]
-       [(eq? '- (first wae_)) (sort (remove-duplicates (append (tmp (second wae_)) (tmp (third wae_)))) symbol<?)])]))
-  (cond
-    [(number? wae) '()]
-    [(symbol? wae) '()]
-    [(list? wae) 
-     (cond
-       [(eq? 'with (first wae)) 
-        
-            (sort (remove-duplicates (remove* (list (first (second wae))) (append (tmp (third wae)) (tmp (second (second wae)))))) symbol<?)
-            ]
-       [(eq? '+ (first wae)) (sort (remove-duplicates (append (tmp (second wae)) (tmp (third wae)))) symbol<?)]
-       [(eq? '- (first wae)) (sort (remove-duplicates (append (tmp (second wae)) (tmp (third wae)))) symbol<?)])]))
+(define (free-ids wae)
+  (type-case  WAE wae
+    [num (n) '()]
+    [id (s) (list s)]
+    [with (x expr body)
+          (sort (remove-duplicates (remove* (list x) 
+                                            (append 
+                                              (free-ids body) 
+                                              (free-ids expr)))) 
+                symbol<?)]
+    [add (l r) (sort (remove-duplicates (append (free-ids l) 
+                                                (free-ids r))) 
+                     symbol<?)]
+    [sub (l r) (sort (remove-duplicates (append (free-ids l) 
+                                                (free-ids r))) 
+                     symbol<?)]))
 
 (test (free-ids (parse '{with {x 5} {+ y z}})) '{y z})
 (test/exn (free-ids (parse '{with x {+ y z} })) "bad syntax")
@@ -95,30 +71,14 @@
 
 ;; binding-ids : WAE -> list-of-sym
 ;; find binding identifiers and list them alphabetical order
-(define (binding-ids wae__)
-  (define wae (reverse_parse wae__))
- (define (tmp wae_)
-   (cond
-    [(number? wae_) '()]
-    [(symbol? wae_) '()]
-    [(list? wae_) 
-     (cond
-       [(eq? 'with (first wae_)) 
-        (sort (remove-duplicates (append (tmp (third wae_)) (list (first (second wae_))))) symbol<?)]
-       [(eq? '+ (first wae_)) (sort (remove-duplicates (append (tmp (second wae_)) (tmp (third wae_)))) symbol<?)]
-       [(eq? '- (first wae_)) (sort (remove-duplicates (append (tmp (second wae_)) (tmp (third wae_)))) symbol<?)])])
-   )
-  (cond
-    [(number? wae) '()]
-    [(symbol? wae) wae]
-    [(list? wae) 
-     (cond
-       [(eq? 'with (first wae)) 
-        
-            (sort (remove-duplicates (append (tmp (third wae)) (list (first (second wae))))) symbol<?)
-            ]
-       [(eq? '+ (first wae)) (sort (remove-duplicates (append (tmp (second wae)) (tmp (third wae)))) symbol<?)]
-       [(eq? '- (first wae)) (sort (remove-duplicates (append (tmp (second wae)) (tmp (third wae)))) symbol<?)])]))
+(define (binding-ids wae)
+   (type-case WAE wae
+    [num (n) '()]
+    [id (s) '()]
+    [with (x expr body)
+          (sort (remove-duplicates (append (binding-ids body) (binding-ids expr) (list x))) symbol<?)]
+    [add (l r) (sort (remove-duplicates (append (binding-ids l) (binding-ids r))) symbol<?)]
+    [sub (l r) (sort (remove-duplicates (append (binding-ids l) (binding-ids r))) symbol<?)]))
 
 (test (binding-ids (parse '{with {x 5} {+ y z}})) '{x})
 (test/exn (binding-ids (parse '{with x {+ y z}})) "bad syntax")
@@ -135,38 +95,27 @@
 
 ;; bound-ids : WAE -> list-of-sym
 ;; find bound identifiers and list them alphabetical order
-(define (bound-ids wae__)
-  (define wae (reverse_parse wae__))
+(define (bound-ids wae)
   (define (intersect l1 l2)
     (cond 
       [(or (empty? l1) (empty? l2)) '()]
       [(eq? (first l1) (first l2)) (cons (first l1) (intersect (rest l1) l2))]
       [(not (eq? (first l1) (first l2))) (intersect (rest l1) l2)]))
-  (define (tmp wae_)
-  (cond
-    [(number? wae_) '()]
-    [(symbol? wae_) (list wae_)]
-    [(list? wae_) 
-     (cond
-       [(eq? 'with (first wae_)) 
-        
-            (sort (remove-duplicates (append (tmp (third wae_)) (tmp (second (second wae_))))) symbol<?)
-            ]
-       [(eq? '+ (first wae_)) (sort (remove-duplicates (append (tmp (second wae_)) (tmp (third wae_)))) symbol<?)]
-       [(eq? '- (first wae_)) (sort (remove-duplicates (append (tmp (second wae_)) (tmp (third wae_)))) symbol<?)])]))
   (define (later wae)
-  (cond
-    [(number? wae) '()]
-    [(symbol? wae) '()]
-    [(list? wae) 
-     (cond
-       [(eq? 'with (first wae)) 
-        
-            (sort (remove-duplicates  (append (tmp (third wae)) (tmp (second (second wae))))) symbol<?)
-            ]
-       [(eq? '+ (first wae)) (sort (remove-duplicates (append (tmp (second wae)) (tmp (third wae)))) symbol<?)]
-       [(eq? '- (first wae)) (sort (remove-duplicates (append (tmp (second wae)) (tmp (third wae)))) symbol<?)])]))
-  (intersect (later wae) (binding-ids wae__))
+    (type-case WAE wae
+               [num (n) '()]
+               [id (s) '()]
+               [with (x expr body)
+                     (sort (remove-duplicates  (append (later body)
+                                                       (later expr)))
+                           symbol<?)]
+               [add (l r) (sort (remove-duplicates (append (later l)
+                                                           (later r)))
+                                symbol<?)]
+               [sub (l r) (sort (remove-duplicates (append (later l)
+                                                           (later r)))
+                                symbol<?)]))
+  (intersect (later wae) (binding-ids wae))
   )
 
 
@@ -184,8 +133,6 @@
 (test (bound-ids (parse 'a)) '())
 
 
-(test (bound-ids (parse '{with {x 5} {+ z {- y z}}})) empty)
-
- 
+(test (bound-ids (parse '{with {x 5} {+ z {- y z}}})) empty) 
 (test (binding-ids (parse '{with {x 5} {+ z {- y x}}})) (list 'x))
 (test (free-ids (parse '{with {x 5} {+ x {- y x}}})) (list 'y))
